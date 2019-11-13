0c0dc7cfb2f2c3c4c2dac323746008ee5c7b5e90
ellieSMASH <eliseaston@gmail.com>
2019-11-13 12:49:53 +0000

    delete obsolete code

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,12 +1,10 @@
 describe("Plane", function () {
   beforeEach(function() {
     plane = new Plane()
-    // airport = new Airport()
     airport = jasmine.createSpyObj('airport', ['prepareForLanding', 'prepareForTakeoff', 'hasSpace']);
   });
 
   it("can land at an airport", function() {
-    // spyOn(airport, 'prepareForLanding')
     plane.land(airport)
     expect(airport.prepareForLanding).toHaveBeenCalled()
   });
