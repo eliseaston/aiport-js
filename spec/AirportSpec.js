dcd13393fc4b355af551a4b1a6301d248f6e1e2d
ellieSMASH <eliseaston@gmail.com>
2019-11-13 12:44:34 +0000

    refactor airport test from spy methods to spy objects. tests pass.

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,26 +1,24 @@
 describe("Airport", function () {
   beforeEach(function() {
-    plane = new Plane();
-    // plane = jasmine.createSpyObj('plane', ['land', 'takeoff']);
+    plane = jasmine.createSpy('plane');
     airport = new Airport();
   });
 
   it("contains a plane when a plane lands", function () {
-    spyOn(plane, 'land').and.callThrough();
-    plane.land(airport)
+    airport.prepareForLanding(plane)
     expect(airport.hangar).toContain(plane)
   });
 
   it("can take off from an airport", function() {
-    spyOn(plane, 'land').and.callThrough();
-    spyOn(plane, 'takeoff').and.callThrough();
+    airport.prepareForLanding(plane)
+    airport.prepareForTakeoff(plane)
     expect(airport.hangar).not.toContain(plane)
   });
 
   it("doesnt allow planes to land when over capacity", function() {
     for(i = 0; i < 10; i++) {
-      plane.land(airport)
+    airport.prepareForLanding(plane)
     }
-    expect(function(){plane.land(airport)}).toThrow("Cannot land!");
+    expect(function(){airport.prepareForLanding(plane)}).toThrow("Cannot land!");
   });
 })
--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,10 +1,12 @@
 describe("Plane", function () {
   beforeEach(function() {
     plane = new Plane()
+    // airport = new Airport()
     airport = jasmine.createSpyObj('airport', ['prepareForLanding', 'prepareForTakeoff', 'hasSpace']);
   });
 
   it("can land at an airport", function() {
+    // spyOn(airport, 'prepareForLanding')
     plane.land(airport)
     expect(airport.prepareForLanding).toHaveBeenCalled()
   });
